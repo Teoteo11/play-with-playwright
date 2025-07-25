05e1d917a41891ab5ca15259ea8ecc715678de48
Teoteo11 <teo.annaro@gmail.com>
2025-07-25 15:37:50 +0200

    feat: init project, install playwright and write load test

---
+++ tests/app.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect, Page } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(`http://localhost:4200/`);
+});
+
+test.describe('App Component', () => {
+  test('Application Load Successfully', async ({ page }) => {
+    await expect(page.locator('app-root')).toBeAttached();
+  });
+
+  test('Router Outlet Tag in HTML', async ({ page }) => {
+    const htmlContent = await page.content();
+    expect(htmlContent).toContain('<router-outlet');
+  });
+}); 
